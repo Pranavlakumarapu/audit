17c866cb5b7a159c4c0373039254c3baf9465aa6
ravikumar <ipca.ravikumar@gmail.com>
2023-02-07 19:17:58 +0530

    menu changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,13 +7,46 @@ export default function Menu() {
 
   return (
     <div >
-    
+{/*
       <center className='links-of-people'>
-     {/* <Link to="/" state={{ data:{name: "Retail"} }} className="retail-link retailtitle"> Retail</Link> */}
      <Link   to="/" state={{ data: { name: "Retail" } }} className="retailtitle">Retail</Link>
       <Link to="/" state={{ data:{name: "Distribution"} }} className="Distributiontitle"> Distribution</Link>
       <Link to="/" state={{ data:{name: "Manufacturing"} }} className="Manufacturingtitle"> Manufacturing</Link>
-      </center>
+      </center> */}
+
+<div className='links'>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm link">
+    <Link   to="/" state={{ data: { name: "Retail" } }} className="retailtitle">
+<div className='teximg-link'>      
+<div><img alt='' src='/images/retailtitleimg.png' className='links-img'/></div>
+<Link   to="/" state={{ data: { name: "Retail" } }} className="retailtitle">Retail</Link>
+</div>
+    </Link>
+    </div>
+
+    <div class="col-sm link">
+    <Link   to="/" state={{ data:{name: "Distribution"} }} className="Distributiontitle">
+<div className='teximg-link'>      
+<div><img alt='' src='/images/distribution.png' className='links-img'/></div>
+<Link   to="/" state={{ data:{name: "Distribution"} }} className="Distributiontitle">Distribution</Link>
+</div>
+    </Link>
+    </div>
+
+
+    <div class="col-sm link">
+    <Link to="/" state={{ data:{name: "Manufacturing"} }} className="Manufacturingtitle">
+<div className='teximg-link'>      
+<div><img alt='' src='/images/manufacture.png' className='links-img'/></div>
+<Link to="/" state={{ data:{name: "Manufacturing"} }} className="Manufacturingtitle">Manufacturing</Link>
+</div>
+    </Link>
+    </div>
+  </div>
+</div>
+</div>
  </div>
   )
 }
