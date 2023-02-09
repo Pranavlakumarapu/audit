8186c1122b878a4ffa98de9e6cf97463ce4a9c7f
ravikumar <ipca.ravikumar@gmail.com>
2023-02-09 06:04:44 +0530

    Bootstrap download

--- src/components/Download/Download.js
+++ src/components/Download/Download.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import './Download.css'
+import React from 'react';
+import './Download.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export default function Download() {
 
   var Downloadfiles = [
+    {
+      title: "Auditsierp18.5.5.9",
+      titleimag: "/images/download-new.gif",
+      Download_link:"/downloads/Auditusierp 18.5.5.9.rar",
+      Date:"Rel-2022-12-12"
+    },
+    {
+      title: "Mysql",
+      titleimag: "imgpath",
+      Download_link:"/downloads/Auditusierp 18.5.5.9.rar",
+      Date:"2022-12-12"
+    }
+    ,  {
+      title: "Auditsierp18.5.59",
+      titleimag: "/images/download-new.gif",
+      Download_link:"/downloads/Auditusierp 18.5.5.9.rar",
+      Date:"Rel-2022-12-12"
+    },
+    {
+      title: "Mysql",
+      titleimag: "imgpath",
+      Download_link:"/downloads/Auditusierp 18.5.5.9.rar",
+      Date:"2022-12-12"
+    },
     {
       title: "Auditsierp18.5.59",
       titleimag: "/images/download-new.gif",
@@ -16,31 +42,33 @@ export default function Download() {
       Download_link:"/downloads/Auditusierp 18.5.5.9.rar",
       Date:"2022-12-12"
     }
-    
   ]
   
   return (
-    <div className='download-body'>
-<center>
-{
+    <div > 
+      <center><h3>Downloads</h3></center>
+      {
+
   Downloadfiles.map((value,index)=>{
-    return<div key={index}>
-<a href={value.Download_link}>
-  <center>
-  <div className='download2'></div>
-    <div className='downloadbox'>
-    <div className='download-heading'> {value.title} <img  src={value.titleimag} alt=''/> </div>
-    <div className='download-date'>{value.Date}</div>
-    </div>
-   
-  </center>
-    </a>
+    return   <div key={index} class="card" style={{ width: '15rem',display:'inline-flex',margin:'10px'}} >
+
+  <div class="card-body">
+    <h5 class="card-title">{value.title} <img  src={value.titleimag} alt=''/></h5>
+    <h6 class="card-subtitle mb-2 text-muted">{value.Date}</h6>
+    {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+    <a href={value.Download_link} class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Download</a>
+  </div>
+
+
+
+
     </div>
   }) 
 }
 
-</center>
 
     </div>
+
+
   )
 }
