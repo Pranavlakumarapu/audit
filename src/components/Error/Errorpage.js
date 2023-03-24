ba5e6759a35899cd97903ab69969cc9177303557
ravikumar <ipca.ravikumar@gmail.com>
2023-03-24 08:11:33 +0530

    error changes  product price drop donwn changes

--- src/components/Error/Errorpage.js
+++ src/components/Error/Errorpage.js
@@ -5,7 +5,9 @@ export default function Errorpage() {
   return (
     <div className='errorpage'>
       
-        <h1 className='error1'><img src='/images/pagenotfound.png' />404 Error Page Not Found <br/>The requested URL was not found on this server.</h1>
+        <h1 className='error1'><img src='/images/pagenotfound.png' /> <br/>
+        Sorry, we couldn't find the page you were looking for. Please try again with 
+        a different URL or contact the website administrator for help. Thank you.</h1>
     </div>
   )
 }
